7a64141575068844de77e1f2fbc60ed9b2615bbf
Roger Braun <davinelulinvega@gmail.com>
2010-10-22 23:36:58 +0200

    Seed for regions and prefectures

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+regions = { "Tôhoku" => %w{Hokkaidô Akita Aomori Fukushima Iwate Miyagi Yamagata}, "Kantô" => %w{Chiba Gunma Ibaraki Kanagawa Saitama Tochigi Tôkyô}, "Chûbu" => %w{Aichi Fukui Gifu Ishikawa Niigata Nagano Shizuoka Toyama Yamanashi}, "Kinki" => %w{Shiga Mie Kyôto Nara Ôsaka Wakayama Hyôgo}, "Chûgoku" => %w{Hiroshima Okayama Shimane Tottori Yamaguchi}, "Shikoku" => %w{Ehime Kagawa Kôchi Tokushima}, "Kyûshû" => %w{Fukuoka Kagoshima Kumamoto Miyazaki Nagasaki Ôita Saga Okinawa} }
+
+regions.each do |name,prefectures|
+  region = Region.create(:name => name)
+  prefectures.each do |prefecture|
+    region.prefectures.create(:name => prefecture)
+  end
+end
+
