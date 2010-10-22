9c472303371008d2c84ec68c8cd871c4ecd159b6
Roger Braun <roger.braun@student.uni-tuebingen.de>
2010-10-22 19:14:43 +0200

    First commit

---
+++ README.mk
@@ -0,0 +1,5 @@
+# Landeskunde 2010/2011
+
+## Über die Seite
+
+Dies ist eine Railsanwendung, die für den Landeskunde-Kurs der Japanologie im WS 10/11 geschrieben wurde. 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WS1011Landeskunde::Application.config.secret_token = 'b9e8a08ee064e282be02695d7b6ebae22bfa430660429253873ebea7b9670e5a7e65f0ffa54900ff0616c82501c266adfc75b5eee1ad548cd776871fbe7ae6d2'
